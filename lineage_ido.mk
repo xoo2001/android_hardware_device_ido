5cfbb272c530832a59eaca9564f5a2dc8b23d75f
xoo2001 <ekora2001@gmail.com>
2021-09-12 20:13:05 +0700

    ido: Update Redfin FP to September

--- lineage_ido.mk
+++ lineage_ido.mk
@@ -36,7 +36,7 @@ PRODUCT_DEVICE := ido
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
-BUILD_FINGERPRINT="Xiaomi/ido/ido:5.1.1/LMY47V/V9.6.1.0.LAICNFD:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ido-user 5.1.1 LMY47V V9.6.1.0.LAICNFD release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
