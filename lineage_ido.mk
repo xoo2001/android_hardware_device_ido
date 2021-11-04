b8ac5935dde6f73b17154dcda98a6e8e1ec6bb02
xoo2001 <ekora2001@gmail.com>
2021-11-04 12:48:37 +0000

    ido: Update Redfin S FP #October
    Signed-off-by: xoo2001 <ekora2001@gmail.com>

--- lineage_ido.mk
+++ lineage_ido.mk
@@ -37,6 +37,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
