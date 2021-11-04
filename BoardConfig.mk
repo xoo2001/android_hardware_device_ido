2b8d816a33c095666f8718a1f4886f4fc9002526
xoo2001 <ekora2001@gmail.com>
2021-11-04 11:56:49 +0000

    ido: Allow ELF prebuilts
    Signed-off-by: xoo2001 <ekora2001@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,3 +110,4 @@ include vendor/xiaomi/ido/BoardConfigVendor.mk
 
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
